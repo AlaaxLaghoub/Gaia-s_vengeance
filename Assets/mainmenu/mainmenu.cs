44b7aa00bfd29b26735b95658baeadaca817aab0
venice <92649256+Huda-bawazir@users.noreply.github.com>
2025-01-22 01:01:58 +0400

    main menu / options / sound
    sound overlay added. buttons and slider that works

--- Assets/mainmenu/mainmenu.cs
+++ Assets/mainmenu/mainmenu.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class mainmenu : MonoBehaviour
 {
+    [Header("volume setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumSlider = null;
+
+    [Header("Confirmation prompt")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
     public void playGame()
     {
         SceneManager.LoadSceneAsync(1);
@@ -15,4 +24,25 @@ public class mainmenu : MonoBehaviour
         Application.Quit();
 
     }
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+
+    }
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("master volume", AudioListener.volume);
+        //show a prompt
+        StartCoroutine(ConfirmationBox()); 
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
+    }
+
+
 }
